28b5de9b75505e4cdec863b150fe936a4aff5942
MinhLD2003 <minhld8403@gmail.com>
2024-04-23 10:26:47 +0700

    Apply JWT auth

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PetProject.API.Services;
 using Project.API.Helper;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +17,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ICustomerService, CustomerService>();
 builder.Services.AddDbContext<DatabaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
-builder.Services.AddAuthentication("BasicAuthentication").AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+//builder.Services.AddAuthentication("BasicAuthentication").AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
 
+var _authkey = builder.Configuration.GetValue<string>("JwtSettings:secretkey");
+builder.Services.AddAuthentication(item =>
+{
+    item.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    item.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(
+    item =>
+    {
+        item.RequireHttpsMetadata = true;
+        item.SaveToken = true;
+        item.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authkey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+
+    }
+    );
 var autoMapper = new MapperConfiguration(item => item.AddProfile(new AutoMapperHandler()));
 IMapper mapper = autoMapper.CreateMapper();
 builder.Services.AddSingleton(mapper);
@@ -29,6 +53,10 @@ builder.Services.AddCors(options =>
                                              .AllowAnyHeader();
                       });
 });
+
+var _jwtSettings = builder.Configuration.GetSection("JWTSettings");
+builder.Services.Configure<JWTSettings>(_jwtSettings);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
